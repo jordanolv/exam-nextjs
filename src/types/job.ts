fb2d31d3d6e0f765d5fe3ae3e639930706fbc146
Jordan <olivier.jordan21@gmail.com>
2026-03-24 11:04:53 +0100

    feat(types): add Job, PinnedJob and CandidatureRecord types

---
+++ src/types/job.ts
@@ -0,0 +1,31 @@
+import type { PrismicDocument, RichTextField, KeyTextField, DateField, GroupField } from "@prismicio/client";
+
+export type JobDocument = PrismicDocument<{
+  title: KeyTextField;
+  description: RichTextField;
+  technologies: GroupField<{ technology: KeyTextField }>;
+  admin_emails: GroupField<{ email: KeyTextField }>;
+  publication_date: DateField;
+}>;
+
+export type Job = {
+  uid: string;
+  title: string;
+  description: RichTextField;
+  technologies: string[];
+  adminEmails: string[];
+  publicationDate: string;
+};
+
+export type PinnedJob = {
+  uid: string;
+  title: string;
+  technologies: string[];
+  pinnedAt: string;
+};
+
+export type CandidatureRecord = {
+  uid: string;
+  title: string;
+  sentAt: string;
+};
